4dd4ace41df61995a2f4cd74853886829c1fe7fd
goga-jpg <goga@5dm.afica>
2026-05-04 13:20:42 +0300

    fix: signal React app loaded to hide WSOD overlay

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Signal to index.html overlay that React app loaded successfully
+window.__REACT_APP_LOADED__ = true;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
